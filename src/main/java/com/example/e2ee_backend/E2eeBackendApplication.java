72cc3b46db0e5b155d3f5cec682ccc27820b8bc0
saikad27 <sairajkad501@icloud.com>
2026-04-11 14:58:24 +0530

    first commit

---
+++ src/main/java/com/example/e2ee_backend/E2eeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.e2ee_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2eeBackendApplication.class, args);
+	}
+
+}
